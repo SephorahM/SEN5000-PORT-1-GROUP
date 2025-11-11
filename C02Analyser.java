c81353f274434e6e18d0f58f3df446812d9e4dfe
SephorahM <st20316678@outlook.cardiffmet.ac.uk>
2025-11-11 17:16:18 +0000

    edit

--- C02Analyser.java
+++ C02Analyser.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class C02Analyser {
     private static final String CSV_FILE = "co2_readings.csv";
 
-    public static void showAnalysisPage(JFrame parentFrame)
+    public static void showAnalysisPage(JFrame parentFrame) {
         JFrame analysisFrame = new JFrame("CO2 Data Analysis");
         analysisFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         analysisFrame.setSize(700,500);
