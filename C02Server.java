00860277c813387cac41e79e38c33727998f1bce
SephorahM <st20316678@outlook.cardiffmet.ac.uk>
2025-11-11 14:40:11 +0000

    server

--- C02Server.java
+++ C02Server.java
@@ -1,3 +1,9 @@
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import javax.swing.*;
+
 public class C02Server {
-    
+    private static final int PORT = 5000;
+    private static final String CSV_FILE = "co2_readings.csv";
 }
